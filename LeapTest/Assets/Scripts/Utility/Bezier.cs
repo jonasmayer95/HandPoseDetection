78a14e4fcb014898da565e22c7806f2b9fa1e02b
Jonas Laptop <jonas.a.mayer.95@googlemail.com>
2016-05-18 21:56:45 +0200

    Started Bezier Implementation
    ajsdhfsjfsdkjfds

---
+++ LeapTest/Assets/Scripts/Utility/Bezier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Bezier {
+
+    public static Vector3 getPoint(float alpha, Vector3[] points)
+    {
+        if (points.Length == 1)
+            return points[0];
+        
+        Vector3[] result = new Vector3[points.Length-1];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = Vector3.Lerp(points[i], points[i+1], alpha);
+        }
+
+        return getPoint(alpha, result);
+    }
+}
