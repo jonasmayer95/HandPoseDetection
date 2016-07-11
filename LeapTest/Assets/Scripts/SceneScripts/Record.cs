a1766038b8e1551d445c35ff42497a0568cbc484
Jonas Home <tum-sg15-mayer-jonas@in.tum.de>
2016-07-11 17:08:24 +0200

    Added Pictures
    asf

---
+++ LeapTest/Assets/Scripts/SceneScripts/Record.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Record : MonoBehaviour {
+	public Text text;
+	public HandObserver hand;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "RRP-Component: " + Mathf.Max (0.0f, Comfort.getRRPComponent(hand.hand)-20);
+	}
+}
